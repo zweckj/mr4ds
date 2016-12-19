f03127d6512aec5fbac28dbcbbefe7d33f4f8932
root <root@munchenmr4ds.2ra1jwl23pie5jdp1zzcsnko5g.cx.internal.cloudapp.net>
2016-12-19 02:21:38 +0000

    update ipyrmd script and ipynbs

--- Instructor-Resources/2-convert-rmd-ipynb.sh
+++ Instructor-Resources/2-convert-rmd-ipynb.sh
@@ -1,7 +1,8 @@
 FILES=../Student-Resources/rmarkdown/*.Rmd
 for f in $FILES
 do
-    ipyrmd --from Rmd --to ipynb $f
+    ipyrmd --from Rmd --to ipynb $f -o "${f%.Rmd}.ipynb" -y
+    mv ../Student-Resources/rmarkdown/*.ipynb ../Student-Resources/notebooks/
 done
 
 
